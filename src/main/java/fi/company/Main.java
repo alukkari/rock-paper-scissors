6177926a4e79805c63bb0ec54453a63b450a3d22
Ari Lukkari <ari.lukkari@gmail.com>
2020-04-17 11:25:04 +0300

    improved code

--- src/main/java/fi/company/Main.java
+++ src/main/java/fi/company/Main.java
@@ -15,41 +15,54 @@ public class Main {
             System.out.println("Choose your weapon:");
             System.out.println("(1)-Rock (2)-Paper (3)-Scissors ?");
             int luku1 = Integer.parseInt(s.nextLine());
-
             int luku2 = (int) (Math.random() * 3)+1;
-            System.out.println(luku2);
-            System.out.println("Computer chose: " + luku2);
+
+            if (luku1 == 1){
+                System.out.println("You chose Rock");
+            }else if (luku1 == 2){
+                System.out.println("You chose Paper");
+            }else if (luku1==3){
+                System.out.println("You chose Scissors");
+            }
+
+            if (luku2 == 1){
+                System.out.println("Computer chose Rock");
+            }else if (luku2 == 2){
+                System.out.println("Computer chose Paper");
+            }else if (luku2==3){
+                System.out.println("Computer chose Scissors");
+            }
 
             if (luku1 == luku2){
-                System.out.println("Tasapeli!");
+                System.out.println("Tie!");
             }else if (luku1==1 && luku2==2){
-                System.out.println("Pelaaja 2 voitti!");
+                System.out.println("Computer Won!");
                 computer++;
             }else if (luku1==2 && luku2==3){
-                System.out.println("Pelaaja 2 voitti!");
+                System.out.println("Computer Won!");
                 computer++;
             }else if (luku1==3 && luku2==1){
-                System.out.println("Pelaaja 2 voitti!");
+                System.out.println("Computer Won!");
                 computer++;
             }else if (luku2==1 && luku1==2){
-                System.out.println("Pelaaja 1 voitti!");
+                System.out.println("You Won!");
                 human++;
             }else if (luku2==2 && luku1==3){
-                System.out.println("Pelaaja 1 voitti!");
+                System.out.println("You Won!");
                 human++;
             }else if (luku2==3 && luku1==1){
-                System.out.println("Pelaaja 1 voitti!");
+                System.out.println("You Won!");
                 human++;
             }
             i++;
         }while (i<3);
 
         if(human>computer){
-            System.out.println("Pelaaja voitti koko matsin!");
+            System.out.println("You Won the match!");
         }else if(human<computer){
-            System.out.println("Tietokone voitti koko matsin!");
+            System.out.println("Computer Won the match!");
         }else if(human == computer){
-            System.out.println("Tasapeli!");
+            System.out.println("Match is a Tie");
         }
     }
 }
